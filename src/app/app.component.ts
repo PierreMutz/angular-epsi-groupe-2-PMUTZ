67d229d3cd346c8d53c997c34d6c06ede3b78838
PierreMutz <pierremutz@hotmail.com>
2021-01-14 12:00:34 +0100

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
-import { Component } from '@angular/core';
-import {SessionService} from './core/services/session.service';
-import {AuthService} from './core/services/auth.service';
-import {Router} from '@angular/router';
+import { Component } from "@angular/core";
+import { SessionService } from "./core/services/session.service";
+import { AuthService } from "./core/services/auth.service";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+  private _snackBar: MatSnackBar;
 
-  constructor(
-    private sessionService: SessionService,
-    private router: Router
-  ) {}
+  constructor(private sessionService: SessionService, private router: Router) {}
 
   get isSignedIn(): boolean {
     return AuthService.isSignedIn;
@@ -21,10 +20,13 @@ export class AppComponent {
 
   signout() {
     // supprimer les données de sessions et retourner à la page de login
-    this.router.navigate(['/auth/signin']).then(() => {
+    this.router.navigate(["/auth/signin"]).then(() => {
       this.sessionService.clear();
       AuthService.user = null;
+      this._snackBar.open("Vous êtes bien déconnecté");
     });
   }
-
+  /*openSnackBar(message: string) {
+    this._snackBar.open("Vous êtes bien déconnecté");
+  }*/
 }
